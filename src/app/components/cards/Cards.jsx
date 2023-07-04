527b391aceb5319aa7929155cf578561bb7a89b6
Márleo Piber da Rosa <marleopr@hotmail.com>
2023-07-04 14:32:38 -0300

    Desenvolvimento modal

--- src/app/components/cards/Cards.jsx
+++ src/app/components/cards/Cards.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import {
   Main,
   CardMovies,
@@ -9,19 +9,39 @@ import {
   PlayButton,
   PlayIcon,
   PosterContainer,
+  ModalOverlay,
+  ModalContent,
+  ModalCloseButton,
+  ModalTitle,
+  VideoPlayer,
+  RangeButton,
+  Button,
+  DownloadIcon,
 } from "./CardsStyled";
 import play from "../../../../public/assets/botao-play.png";
+import cloudIcon from "../../../../public/assets/cloud-download.svg";
 
 const Context = createContext();
 
 const Cards = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const openModal = (video) => {
+    setSelectedVideo(video);
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   return (
     <Main>
       <CardMovies>
         <ImagesPoster>
           {require("./videos.json").videos.map((video, index) => (
             <div key={index}>
-              <PosterContainer>
+              <PosterContainer onClick={() => openModal(video)}>
                 <PosterPath src={video.image} alt={video.title} />
                 <PlayButton className="play-button">
                   <PlayIcon src={play} alt="Play" />
@@ -32,6 +52,44 @@ const Cards = () => {
           ))}
         </ImagesPoster>
       </CardMovies>
+      {modalOpen && selectedVideo && (
+        <ModalOverlay>
+          <ModalContent>
+            <ModalCloseButton onClick={closeModal}>x</ModalCloseButton>
+            <ModalTitle>
+              <span className="title">Webnar:</span>
+              <span>{selectedVideo.title}</span>
+            </ModalTitle>
+            <iframe
+              width="400"
+              height="315"
+              src={selectedVideo.url}
+              frameBorder="0"
+              allow="autoplay; encrypted-media"
+              allowFullScreen
+            ></iframe>{" "}
+            <strong>Descrição</strong>
+            <h6 style={{ marginTop: "2%" }} className="dividing-line"></h6>
+            <p>{selectedVideo.description}</p>
+            <strong>Downloads</strong>
+            <h6 style={{ marginTop: "2%" }} className="dividing-line"></h6>
+            <RangeButton>
+              <Button className="xls-button">
+                <DownloadIcon src={cloudIcon} />
+                Spreadsheet.xls
+              </Button>
+              <Button className="doc-button">
+                <DownloadIcon src={cloudIcon} />
+                Document.doc
+              </Button>
+              <Button className="ppt-button">
+                <DownloadIcon src={cloudIcon} />
+                Presentation.ppt
+              </Button>
+            </RangeButton>
+          </ModalContent>
+        </ModalOverlay>
+      )}
     </Main>
   );
 };
--- src/app/components/cards/CardsStyled.jsx
+++ src/app/components/cards/CardsStyled.jsx
@@ -11,11 +11,11 @@ export const Main = styled.div`
 `;
 
 export const CardMovies = styled.div`
-  margin: 10px;
-  padding: 5px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin: 10px;
+  padding: 5px;
   border-radius: 10px;
   h2 {
     padding: 10px;
@@ -35,9 +35,10 @@ export const ImagesPoster = styled.div`
   cursor: pointer;
 `;
 export const PosterContainer = styled.div`
+  position: relative;
+  border: solid red;
   border-radius: 10px;
   box-shadow: 0 5px 5px 0 rgba(0, 0, 0, 0.2), 0 10px 20px 0 rgba(0, 0, 0, 0.19);
-  position: relative;
   &:hover {
     .play-button {
       opacity: 1;
@@ -60,18 +61,18 @@ export const Title = styled.p`
 `;
 
 export const PlayButton = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
   display: flex;
   align-items: center;
   justify-content: center;
+  position: absolute;
   width: 80px;
   height: 80px;
-  border-radius: 50%;
   font-size: 40px;
   color: #fff;
+  top: 50%;
+  left: 50%;
+  border-radius: 50%;
+  transform: translate(-50%, -50%);
   opacity: 0;
   transition: opacity 0.3s;
 `;
@@ -79,4 +80,154 @@ export const PlayButton = styled.div`
 export const PlayIcon = styled(Image)`
   width: 60px;
   height: 60px;
+`;
+export const ModalOverlay = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  border: solid greenyellow;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+export const ModalContent = styled.div`
+  position: relative;
+  width: 450px;
+  height: 670px;
+  border-radius: 10px;
+  border-top: solid #0084ff;
+  background-color: #fff;
+  iframe {
+    width: 100%;
+    margin-bottom: 10px;
+  }
+  .dividing-line {
+    text-align: center;
+    width: 90%;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+  }
+  strong {
+    margin: 10px;
+  }
+  p {
+    margin-left: 10px;
+    margin-bottom: 10px;
+  }
+  h6 {
+    margin-left: 10px;
+    margin-bottom: 5px;
+  }
+`;
+export const ModalCloseButton = styled.button`
+  position: absolute;
+  outline: none;
+  font-size: 20px;
+  top: 1px;
+  right: 10px;
+  border: none;
+  color: #555555;
+  background-color: transparent;
+  cursor: pointer;
+  &:hover {
+    color: #0084ff;
+  }
+`;
+export const ModalTitle = styled.h2`
+  font-weight: 600;
+  padding: 35px 60px 20px 60px;
+  .title {
+    color: #0084ff;
+    margin-right: 5px;
+  }
+`;
+
+export const RangeButton = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  position: relative;
+  font-size: 10px;
+  border: none;
+  border-radius: 5px;
+  padding: 5px 10px;
+  padding-left: 30px;
+  margin: 5px;
+  cursor: pointer;
+  &.xls-button {
+    background: rgb(159, 239, 223);
+    background: linear-gradient(
+      90deg,
+      rgba(159, 239, 223, 1) 25%,
+      rgba(194, 244, 234, 1) 25%
+    );
+    &:hover {
+      color: #000000;
+    }
+    &:active {
+      background: rgb(159, 239, 223);
+      background: linear-gradient(
+        90deg,
+        rgba(159, 239, 223, 1) 100%,
+        rgba(159, 239, 223, 1) 100%
+      );
+    }
+    color: #005c2c;
+  }
+  &.doc-button {
+    background: rgb(161, 217, 255);
+    background: linear-gradient(
+      90deg,
+      rgba(161, 217, 255, 1) 25%,
+      rgba(194, 230, 255, 1) 25%
+    );
+    color: #0a6bc5;
+    &:hover {
+      color: #000000;
+    }
+    &:active {
+      background: rgb(161, 217, 255);
+      background: linear-gradient(
+        90deg,
+        rgba(161, 217, 255, 1) 100%,
+        rgba(194, 230, 255, 1) 100%
+      );
+    }
+  }
+  &.ppt-button {
+    background: rgb(255, 241, 160);
+    background: linear-gradient(
+      90deg,
+      rgba(255, 241, 160, 1) 25%,
+      rgba(255, 248, 208, 1) 25%
+    );
+    color: #9d870d;
+    &:hover {
+      color: #000000;
+    }
+    &:active {
+      background: rgb(255, 241, 160);
+      background: linear-gradient(
+        90deg,
+        rgba(255, 241, 160, 1) 100%,
+        rgba(255, 241, 160, 1) 100%
+      );
+    }
+  }
+`;
+export const DownloadIcon = styled(Image)`
+  width: 18px;
+  height: 18px;
+  position: absolute;
+  top: 50%;
+  left: 5px;
+  right: 5px;
+  transform: translateY(-50%);
 `;
