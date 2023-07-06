07377114e64cbc804f6124f416311c35717de0a4
Márleo Piber da Rosa <marleopr@hotmail.com>
2023-07-05 22:30:47 -0300

    Desenvolvimento Footer

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -1,6 +1,80 @@
+"use client";
+import { createContext } from "react";
+import Image from "next/image";
+import logo from "public/assets/leadster_270.gif";
+import linkedin from "public/assets/linkedin.png";
+import facebook from "public/assets/facebook.png";
+import instagram from "public/assets/instagram.png";
+import {
+  Main,
+  RangeLogo,
+  Logo,
+  Menu,
+  Socials,
+  InfoFooter,
+  HeaderDivider,
+} from "./FooterStyled";
+const Context = createContext();
+
 const Footer = () => {
-    return (
-        <div>Footer</div>
-    )
-}
-export default Footer
+  return (
+    <Main>
+      <RangeLogo>
+        <Logo>
+          <Image src={logo} alt="Logotipo da Leadster" />
+        </Logo>
+      </RangeLogo>
+      <p>Transformando visitantes em clientes.</p>
+      <Menu>
+        <ul>
+          <strong>Links Principais</strong>
+          <li>Home</li>
+          <li>Ferramenta</li>
+          <li>Preço</li>
+          <li>Contato</li>
+        </ul>
+        <ul>
+          <strong>Cases</strong>
+          <li>Geração de Leads B2B</li>
+          <li>Geração de Leads em Software</li>
+          <li>Geração de Leads em Imobiliária</li>
+          <li>Cases de Sucesso</li>
+        </ul>
+        <ul>
+          <strong>Materiais</strong>
+          <li>Blog</li>
+          <li>Parceria com Agências</li>
+          <li>Guia Definitivo do Marketing</li>
+          <li>Materiais Gratuitos</li>
+        </ul>
+        <ul>
+          <strong>Siga a Leadster</strong>
+          <span className="fotosocial">
+            <Socials src={linkedin} alt="Linkedin" width={50} />
+            <Socials src={facebook} alt="Facebook" width={50} />
+            <Socials src={instagram} alt="Instagram" width={50} />
+          </span>
+          <li>
+            <span className="address">E-mail:</span> contato@leadster.com.br
+          </li>
+          <li>
+            <span className="address">Telefone:</span> (42) 98828-9851
+          </li>
+        </ul>
+      </Menu>
+      <HeaderDivider />
+
+      <InfoFooter>
+        <span>
+          Copyright © 2015 - 2022 Todos os direitos reservados |
+          <span className="info-footer">Leadster</span>
+        </span>
+        <p>
+          Rua José Loureiro, 464 - Centro - Curitiba PR - CEP: 80010-000 |
+          Termos de uso
+        </p>
+      </InfoFooter>
+    </Main>
+  );
+};
+export default Footer;
--- src/app/components/footer/FooterStyled.jsx
+++ src/app/components/footer/FooterStyled.jsx
@@ -1,1 +1,76 @@
+import Image from "next/image";
 import styled from "styled-components";
+
+export const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 0px;
+  p {
+    color: #808080;
+  }
+  .address {
+    color: black;
+  }
+`;
+
+export const RangeLogo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin: 20px;
+`;
+
+export const Logo = styled.div``;
+export const Menu = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  width: 80%;
+  padding: 30px;
+  .fotosocial {
+    display: flex;
+    flex-direction: row;
+    margin-top: 15px;
+    margin-bottom: 15px;
+  }
+  li {
+    margin-top: 25px;
+    color: #858da8;
+    &:hover {
+      cursor: pointer;
+      color: #007eff;
+    }
+  }
+`;
+export const Socials = styled(Image)`
+  padding: 5px;
+  margin: 0 5px 0 0;
+  border-radius: 25px;
+  background-color: #e2e2e2;
+  &:hover {
+    background-color: #007eff;
+    cursor: pointer;
+  }
+`;
+export const InfoFooter = styled.p`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  width: 80%;
+  margin-bottom: 20px;
+  .info-footer {
+    color: #007eff;
+    margin-left: 5px;
+  }
+`;
+export const HeaderDivider = styled.div`
+  width: 100%;
+  height: 1px;
+  margin: 20px -10% 5px -10%;
+  margin-bottom: 20px;
+  opacity: 0.2;
+  background-color: #858da8;
+`;
